f4d2575315c513c301f8a2a49aab1ecc6e103220
AlexisLavigne <70544766+AlexisLavigne@users.noreply.github.com>
2020-09-14 15:23:14 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -10,11 +10,13 @@ def average(a: float, b: float, c: float) -> float:
 
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    return * math.pi/180
+    return (angle_degs + (angle_mins + angle_secs/60)/60) * math.pi/180
 
 
 def to_degrees(angle_rads: float) -> tuple:
-    return 0.0, 0.0, 0.0 (angle_rads * 180/math.pi)
+    angle = (angle_rads * 360)/(2 * math.pi)
+    degrees, minutes, seconds = math.floor(angle), math.floor(angle / 60), math.floor(angle / 3600)
+    return degrees, minutes, seconds
 
 
 def to_celsius(temperature: float) -> float:
